08e16135cfc897c29f62606d3f53ef6660c6b195
Aditya Sakpal <112710558+Aditya-Sakpal@users.noreply.github.com>
2024-04-14 09:12:53 +0530

    Update server.ts

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,6 +1,17 @@
 import express, { Request, Response } from 'express';
+import connect from './db';
+import bcrypt from "bcryptjs";
+import nodemailer from 'nodemailer';
+import { User, SelectedUsers, Group, AiChat, Meeting, OnlineUser } from './model';
 import cors from 'cors';
-
+import path from 'path';
+import multer from 'multer';
+import fs from 'fs-extra';
+import OpenAI from "openai";
+import Anthropic from '@anthropic-ai/sdk';
+import axios from 'axios';
+import Replicate from "replicate";
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 const app = express();
 const port = 4000;
 app.use(express.json())
