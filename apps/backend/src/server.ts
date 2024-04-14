b998d286f9d1d648ff59cb37214c78f6af78d020
Aditya Sakpal <112710558+Aditya-Sakpal@users.noreply.github.com>
2024-04-14 09:02:56 +0530

    Update server.ts

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,14 +1,1 @@
-import express, { Request, Response } from 'express';
 
-const app = express();
-const port = 4000;
-app.use(express.json())
-app.use(cors())
-
-app.get('/', async (req: Request, res: Response) => {
-  res.send('Hello, TypeScript!');
-});
-
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
